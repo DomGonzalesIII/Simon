b100ced99898a8373b7821f8c0768273b4adce59
DGIII <34903375+DomGonzalesIII@users.noreply.github.com>
2020-05-18 00:14:04 -0500

    Finished Commit
    Added two features not in the demo app: 1) game over dosn't occur if 
    button clicked while not actually playing and 2) entire pattern 
    displays, like actual game.

--- game.js
+++ game.js
@@ -22,25 +22,32 @@ $(function () {
   // event listener for player's response
   $('.btn').on('click', function () {
 
-    // add color user has selected to user's pattern
-    let userChosenColor = this.id;
-    userClickedPattern.push(userChosenColor);
+    // only want buttons to do something if game has started
+    if (gameStarted) {
+      // add color user has selected to user's pattern
+      let userChosenColor = this.id;
+      userClickedPattern.push(userChosenColor);
 
-    // play button's sound and animate color as confirmation of press
-    playSound(userChosenColor);
-    animatePress(userChosenColor);
+      // play button's sound and animate color as confirmation of press
+      playSound(userChosenColor);
+      animatePress(userChosenColor);
 
-    checkAnswer(level);
+      checkAnswer(userClickedPattern.length - 1);
+    }
   });
 
   function nextSequence() {
+
+    userClickedPattern = [];
+    level++;
+
+    $('#level-title').text(`Level ${level}`);
 
     // selects a color at random and adds it to the game pattern
     let randomNumber = Math.floor(Math.random() * 4);
     let randomChosenColor = gameColors[randomNumber];
     gamePattern.push(randomChosenColor);
 
-    // TODO: add loop here to go thru whole game pattern not just current choice
     animateGamePattern();
     counter = 0;
   }
@@ -74,18 +81,34 @@ $(function () {
 
   function checkAnswer(currentLevel) {
     if (userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
-      console.log('correct');
+      if (userClickedPattern.length === gamePattern.length) {
+        console.log('correct');
 
-      level++;
-
-      setTimeout(function () {
-        // update the h1 with this change in the value of level
-        $('#level-title').text(`Level ${level}`);
-        nextSequence();
-      }, 1000);
+        setTimeout(function () {
+          // update the h1 with this change in the value of level
+          $('#level-title').text(`Level ${level}`);
+          nextSequence();
+        }, 1000);
+      }
     } else {
-      console.log('wrong');
+      playSound('wrong');
+      $('body').addClass('game-over');
+      setTimeout(function () {
+        $('body').removeClass('game-over');
+      }, 200);
+
+      $('#level-title').text('Game Over, Press Any Key to Restart');
+
+      startOver();
     }
+  }
+
+  function startOver() {
+    gamePattern = [];
+    userClickedPattern = [];
+    counter = 0;
+    gameStarted = false;
+    level = 0;
   }
 
 });
